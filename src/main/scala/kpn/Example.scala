a6891e85406e4539496207d1c0157026cb33ac44
Philipp Ruemmer <ph_r@gmx.net>
2024-05-23 23:14:24 +0200

    more

--- src/main/scala/kpn/Example.scala
+++ src/main/scala/kpn/Example.scala
@@ -23,16 +23,6 @@ object FibonacciVerify extends App {
 
 }
 
-object ScheduleExample extends App {
-
-  import Scheduler._
-
-  println(progEpsSchedule(ExampleProg2Unsafe.procA).toSchedule)
-  println(progEpsSchedule(ExampleProg2Unsafe.procB).toSchedule)
-  println(progEpsSchedule(ExampleProgSum.procInc).toSchedule)
-
-}
-
 object ExampleProgFib {
 
   import KPN._
